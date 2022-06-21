61cf4d736b31e041a896c598087142577e445588
unknown <mbakweangela96@gmail.com>
2022-06-21 16:30:18 +0100

    first push

--- algorithms/challenge-1.js
+++ algorithms/challenge-1.js
@@ -1,4 +1,7 @@
 function secondLargest(arr) {
-  
+  let sortedArr=arr.sort((a,b)=>{return b-a})
+  return sortedArr[1];
 }
 module.exports = secondLargest
+
+console.log(secondLargest([6, 3, 0, 30, 7]))
