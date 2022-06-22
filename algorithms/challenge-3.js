86f26c85d72bbe37be7f6d9ec882c63876d04f58
Angela <105223543+Macclare@users.noreply.github.com>
2022-06-22 16:22:30 +0100

    git commit -m
    Assessment done

--- algorithms/challenge-3.js
+++ algorithms/challenge-3.js
@@ -1,5 +1,14 @@
 function sumMix(arr) {
-   
+   let sum = 0;
+  let inOnlyArr = arr.map((item) => {
+    return parseInt(item, 10);
+  });
+  inOnlyArr.map((item) => {
+    return (sum += item);
+  });
+  return sum;
  }
 
   module.exports = sumMix
+
+console.log(sumMix([9, 3, "7", "3"]));
