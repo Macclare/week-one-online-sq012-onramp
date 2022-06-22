1105c0081a9b8ec92ab7badf6c44e961b30cf8aa
Angela <105223543+Macclare@users.noreply.github.com>
2022-06-22 16:24:39 +0100

    git commit -m
    Assessment done

--- algorithms/challenge-5.js
+++ algorithms/challenge-5.js
@@ -1,5 +1,9 @@
 function filterList(arr) {
- 
+ return arr.filter(function (item) {
+    return typeof item === "number";
+  });
  }
 
   module.exports =filterList
+
+console.log(filterList([1, 2, "a", "b"]));
