779ce68655e74f59904638bb45834cfd2046afa0
Angela <105223543+Macclare@users.noreply.github.com>
2022-06-22 16:23:41 +0100

    git commit -m
    Assessment done

--- algorithms/challenge-4.js
+++ algorithms/challenge-4.js
@@ -1,5 +1,15 @@
 function sumTwoSmallestNums(arr) {
-    
+   let sum = 0;
+  let filteredArr = arr.filter((elem) => {
+    return elem > 0;
+  });
+  let sortedFilteredArr = filteredArr.sort((a, b) => {
+    return a - b;
+  });
+  sum = sortedFilteredArr[0] + sortedFilteredArr[1];
+  return sum; 
  
  }
   module.exports =sumTwoSmallestNums
+
+console.log(sumTwoSmallestNums([19, 5, 42, 2, 77]));
